a6b2c832486fa81644d365236ce52d8aa5859b81
Troys-Code <troy.schultz@chargepoint.com>
2023-06-29 14:17:15 -0700

    Implemented the code to pass all my test cases

--- 1_Android/asgn1/Assignment1/src/main/kotlin/CalendarArray.kt
+++ 1_Android/asgn1/Assignment1/src/main/kotlin/CalendarArray.kt
@@ -10,12 +10,33 @@
 */
 
 import java.time.YearMonth
+import kotlin.math.ceil
 
 class CalendarArray {
-  /**
-   * See assignment specification.
-   */
-  fun generate(month: YearMonth): Array<IntArray> {
-    TODO("Not yet implemented")
+  fun generate(yearMonth: YearMonth): Array<IntArray> {
+    val daysInMonth = yearMonth.lengthOfMonth()
+    var firstDayOfWeek = yearMonth.atDay(1).dayOfWeek.value
+
+    firstDayOfWeek = if (firstDayOfWeek == 7) 0 else firstDayOfWeek
+    val weeks = ceil((daysInMonth + firstDayOfWeek - 1) / 7.0).toInt().coerceAtLeast(6)
+    val calendarArray = Array(weeks) { IntArray(7) }
+
+    var day = 1
+    for (week in 0 until weeks) {
+      for (dayOfWeek in 0 until 7) {
+        if (week == 0 && dayOfWeek < firstDayOfWeek) {
+          calendarArray[week][dayOfWeek] = 0
+        } else if (day <= daysInMonth) {
+          calendarArray[week][dayOfWeek] = day++
+        } else {
+          calendarArray[week][dayOfWeek] = 0
+        }
+      }
+    }
+
+    return calendarArray
   }
 }
+
+
+
