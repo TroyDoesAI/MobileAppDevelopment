b05ab37e29896c52c987326a3ea8bbb7066d72d9
Troys-Code <troy.schultz@chargepoint.com>
2023-06-29 14:27:48 -0700

    Refactored Code, Added Credit and created Doc Strings -> Ensured All Tests Still Pass
    Credit :
    - Auto Docstrings Credit : https://plugins.jetbrains.com/plugin/14778-kdoc-er--kotlin-doc-generator
    
    - Fixing my summary Wording and Grammar Ref: https://www.grammarly.com/
    
    - Leap Years more in depth Ref : https://www.almanac.com/content/when-next-leap-year

--- 1_Android/asgn1/Assignment1/src/main/kotlin/CalendarArray.kt
+++ 1_Android/asgn1/Assignment1/src/main/kotlin/CalendarArray.kt
@@ -8,35 +8,47 @@
 #
 #######################################################################
 */
-
-import java.time.YearMonth
+import java.time.YearMonth // Ref : https://www.geeksforgeeks.org/java-time-yearmonth-class-in-java/#
 import kotlin.math.ceil
 
-class CalendarArray {
+class CalendarArray { // Auto Docstrings Credit : https://plugins.jetbrains.com/plugin/14778-kdoc-er--kotlin-doc-generator, Fixing my summary Wording and Grammar Ref: https://www.grammarly.com/, UNDERSTANDING LEAP YEARS Ref : https://www.almanac.com/content/when-next-leap-year
+  /**
+   * The `generate` function creates a two-dimensional array (6x7) representing a calendar month for a specific YearMonth.
+   * Each element of the array corresponds to a day of the month, with zeroes representing days that fall outside the
+   * given month (either from the previous month or the next month).
+   *
+   * @param yearMonth: The year and month for which the calendar should be generated.
+   *
+   * Understanding Leap Years:
+   * A leap year is a year, occurring once every four years, which has 366 days including 29 February as an intercalary day.
+   * This is necessary because the actual length of a year is not 365 days, as we usually consider, but approximately
+   * 365.2425 days. To align the calendar year with the solar year (the length of time it takes the Earth to orbit around
+   * the Sun), we add an extra day, February 29, almost every four years.
+   *
+   * The function automatically handles leap years when generating the calendar. For instance, when you pass
+   * YearMonth.of(2024, 2) as an argument, it will correctly generate a February calendar with 29 days because 2024 is a leap year.
+   *
+   * @return calendarArray: A two-dimensional array representing the calendar for the provided YearMonth.
+   * The rows represent weeks (up to six, enough to cover any possible month), and the columns represent days of the week
+   * from Monday (column 0) to Sunday (column 6).
+   * The array is filled with day numbers for the current month, starting with 1, and zeros for the days that fall outside
+   * of the current month.
+   *
+   */
   fun generate(yearMonth: YearMonth): Array<IntArray> {
     val daysInMonth = yearMonth.lengthOfMonth()
-    var firstDayOfWeek = yearMonth.atDay(1).dayOfWeek.value
-
-    firstDayOfWeek = if (firstDayOfWeek == 7) 0 else firstDayOfWeek
-    val weeks = ceil((daysInMonth + firstDayOfWeek - 1) / 7.0).toInt().coerceAtLeast(6)
+    val firstDayOfWeek = (yearMonth.atDay(1).dayOfWeek.value % 7) // 0-6
+    val weeks = ceil((daysInMonth + firstDayOfWeek - 1) / 7.0).toInt().coerceAtLeast(6) // coerceAtLeast : Ensures that this value is not less than the specified minimumValue. Reference: https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.ranges/coerce-at-least.html
     val calendarArray = Array(weeks) { IntArray(7) }
-
     var day = 1
-    for (week in 0 until weeks) {
+    for (week in 0 until weeks)
       for (dayOfWeek in 0 until 7) {
-        if (week == 0 && dayOfWeek < firstDayOfWeek) {
-          calendarArray[week][dayOfWeek] = 0
-        } else if (day <= daysInMonth) {
-          calendarArray[week][dayOfWeek] = day++
-        } else {
-          calendarArray[week][dayOfWeek] = 0
+        calendarArray[week][dayOfWeek] = when {
+          week == 0 && dayOfWeek < firstDayOfWeek -> 0
+          day <= daysInMonth -> day++
+          else -> 0
         }
       }
-    }
-
     return calendarArray
   }
 }
-
-
-
