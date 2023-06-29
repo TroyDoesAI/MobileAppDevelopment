928fbcf3d08efd28d3fdabcba4fbdd68033d1871
Troys-Code <troy.schultz@chargepoint.com>
2023-06-29 13:48:34 -0700

    Restructured Test Class, added addition tests for floating point equivalents where I needed to use a Delta , also added check for large numbers and multiple operators

--- 1_Android/asgn1/Assignment1/src/test/kotlin/PostfixCalculatorTest.kt
+++ 1_Android/asgn1/Assignment1/src/test/kotlin/PostfixCalculatorTest.kt
@@ -9,44 +9,79 @@
 #######################################################################
 */
 
-import org.junit.jupiter.api.Assertions.assertEquals
+// Credits : InfixCalculator Test from David C. Harrison's CSE118 example code
+
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+import kotlin.test.fail
+
+/*
+ * Testing class for the PostfixCalculator.
+ *
+ * Here's a concise explanation of the testing order for the PostfixCalculator suite:
+ *
+ * 1. Malformed Expressions:
+ *    - Tests for handling malformed expressions.
+ *    - Covers cases like empty expressions, missing operators/operands, and excessive whitespace.
+ *    - Ensures appropriate exceptions are thrown.
+ *
+ * 2. Basic Integer Operations:
+ *    - Tests integer arithmetic operations: addition, subtraction, multiplication, division, and power.
+ *    - Verifies correct integer results.
+ *
+ * 3. Floating-Point Calculations:
+ *    - Tests floating-point calculations, considering precision issues using a delta (hardcoded precision acceptance value).
+ *    - Covers addition, subtraction, multiplication, division, and power operations.
+ *    - Uses `assertEquals` with delta for acceptable value range.
+ *
+ * 4. Edge Cases and Error Scenarios:
+ *    - Tests special cases, errors, and boundary conditions.
+ *    - Covers division by zero, invalid operators, non-numeric operands, large numbers, and consecutive operators.
+ *    - Validates expected exceptions and correct results.
+ */
 
 class PostfixCalculatorTest {
+  // Step 1: Test for malformed expressions
   @Test
-  fun `test addition`() {
-    val calculator = PostfixCalculator()
-    val result = calculator.parse("5 3 +")
-    assertEquals(8.0, result)
+  internal fun `malformed expression - empty expression`() {
+    assertFails { PostfixCalculator().parse("") }
+  }
+  @Test
+  internal fun `malformed expression - missing operator`() {
+    assertThrows<IllegalArgumentException> { PostfixCalculator().parse("1 1") }
+  }
+  @Test
+  internal fun `malformed expression - missing operand`() {
+    assertThrows<IllegalArgumentException> { PostfixCalculator().parse("1 +") }
+  }
+  @Test
+  internal fun `malformed expression - extra whitespace`() {
+    assertEquals(2.0, PostfixCalculator().parse("  1   1  +   "))
+  }
+  @Test
+  internal fun `malformed expression - excessive whitespace`() {
+    assertEquals(5.0, PostfixCalculator().parse("2   3   +"))
   }
 
+  // Step 2: Test for basic integer operations
   @Test
-  fun `test subtraction`() {
-    val calculator = PostfixCalculator()
-    val result = calculator.parse("10 3 -")
-    assertEquals(7.0, result)
+  internal fun `integer addition`() {
+    assertEquals(2.0, PostfixCalculator().parse("1 1 +"))
   }
-
   @Test
-  fun `test multiplication`() {
-    val calculator = PostfixCalculator()
-    val result = calculator.parse("4 5 *")
-    assertEquals(20.0, result)
+  internal fun `integer subtraction`() {
+    assertEquals(-1.0, PostfixCalculator().parse("1 2 -"))
   }
-
   @Test
-  fun `test division`() {
-    val calculator = PostfixCalculator()
-    val result = calculator.parse("12 3 /")
-    assertEquals(4.0, result)
+  internal fun `integer multiplication`() {
+    assertEquals(42.0, PostfixCalculator().parse("2 21 *"))
   }
-
   @Test
-  internal fun `division by zero`() {
-    assertThrows<IllegalArgumentException> { PostfixCalculator().parse("1 0 /") }
+  internal fun `integer division`() {
+    assertEquals(0.0, PostfixCalculator().parse("0 1 /"))
   }
-
   @Test
   internal fun `integer power operation`() {
     assertEquals(8.0, PostfixCalculator().parse("2 3 ^"))
@@ -54,14 +89,63 @@ class PostfixCalculatorTest {
     assertEquals(1.0, PostfixCalculator().parse("1 100000 ^"))
   }
 
+  // Step 3: Test for floating-point calculations
+  // In Kotlin, assertEquals method has a delta parameter to specify the maximum acceptable difference between expected & actual values.
+  // Reference: https://kotlinlang.org/api/latest/kotlin.test/kotlin.test/assert-equals.html
+  @Test
+  internal fun `floating-point addition`() {
+    val expected = 0.1
+    val actual = PostfixCalculator().parse("0.05 0.05 +")
+    val delta = 0.000001 // Specify the maximum acceptable difference (delta) here
+    assertEquals(expected, actual, delta)
+  }
+  @Test
+  internal fun `floating-point subtraction`() {
+    val expected = 0.1
+    val actual = PostfixCalculator().parse("0.3 0.2 -")
+    val delta = 0.000001 // Specify the maximum acceptable difference (delta) here
+    assertEquals(expected, actual, delta)
+  }
+  @Test
+  internal fun `floating-point multiplication`() {
+    val expected = 0.012
+    val actual = PostfixCalculator().parse("0.03 0.4 *")
+    val delta = 0.000001 // Specify the maximum acceptable difference (delta) here
+    assertEquals(expected, actual, delta)
+  }
+  @Test
+  internal fun `floating-point division`() {
+    val expected = 0.3333333333333333
+    val actual = PostfixCalculator().parse("1.0 3.0 /")
+    val delta = 0.000001 // Specify the maximum acceptable difference (delta) here
+    assertEquals(expected, actual, delta)
+  }
+  @Test
+  internal fun `floating point power operation`() {
+    assertEquals(8.0, PostfixCalculator().parse("2.0 3.0 ^")) // Both are floating point
+    assertEquals(1.0, PostfixCalculator().parse("10.0 0 ^")) // Base is float, exponent int
+    assertEquals(1.0, PostfixCalculator().parse("1 100000.0 ^")) // Base is int, exponent is a float
+  }
+
+  // Step 4: Test for edge cases and error scenarios
+  @Test
+  internal fun `division by zero`() {
+    assertThrows<IllegalArgumentException> { PostfixCalculator().parse("1.0 0.0 /") }
+  }
   @Test
   internal fun `invalid operator`() {
     assertThrows<IllegalArgumentException> { PostfixCalculator().parse("1 1 $") }
   }
-
   @Test
   internal fun `non-numeric operands`() {
-    assertThrows<NumberFormatException> { PostfixCalculator().parse("1a 2 +") }
+    assertThrows<IllegalArgumentException> { PostfixCalculator().parse("1a 2 +") }
   }
-
+  @Test
+  internal fun `large numbers`() {
+    assertEquals(1.0, PostfixCalculator().parse("10000000000 10000000000 /"))
+  }
+  @Test
+  internal fun `multiple consecutive operators`() {
+    assertThrows<IllegalArgumentException> { PostfixCalculator().parse("1 2 + -") }
+  }
 }
