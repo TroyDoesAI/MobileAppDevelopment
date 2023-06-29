d3bf57e7386dad52c37f871871a631c129de3cf0
Troys-Code <troy.schultz@chargepoint.com>
2023-06-29 14:10:04 -0700

    Added Failing Test Case for leap year and non leap years
    Matrices created using my phones calculator for ref
    They will fail until I create the generate() function

--- 1_Android/asgn1/Assignment1/src/test/kotlin/CalendarArrayTest.kt
+++ 1_Android/asgn1/Assignment1/src/test/kotlin/CalendarArrayTest.kt
@@ -28,4 +28,46 @@ internal class CalendarArrayTest {
     )
     Assertions.assertArrayEquals(expectedArray, calendarArray)
   }
+
+  @Test
+  fun generateCalendarArrayForLeapYearFeb2024() {
+    val calendarArray: Array<IntArray> = CalendarArray().generate(YearMonth.of(2024, 2))
+    val expectedArray = arrayOf(
+      intArrayOf(0, 0, 0, 0, 1, 2, 3),
+      intArrayOf(4, 5, 6, 7, 8, 9, 10),
+      intArrayOf(11, 12, 13, 14, 15, 16, 17),
+      intArrayOf(18, 19, 20, 21, 22, 23, 24),
+      intArrayOf(25, 26, 27, 28, 29, 0, 0),
+      intArrayOf(0, 0, 0, 0, 0, 0, 0)
+    )
+    Assertions.assertArrayEquals(expectedArray, calendarArray)
+  }
+
+  @Test
+  fun generateCalendarArrayForNonLeapYear() {
+    val calendarArray: Array<IntArray> = CalendarArray().generate(YearMonth.of(2023, 2))
+    val expectedArray = arrayOf(
+      intArrayOf(0, 0, 0, 1, 2, 3, 4),
+      intArrayOf(5, 6, 7, 8, 9, 10, 11),
+      intArrayOf(12, 13, 14, 15, 16, 17, 18),
+      intArrayOf(19, 20, 21, 22, 23, 24, 25),
+      intArrayOf(26, 27, 28, 0, 0, 0, 0),
+      intArrayOf(0, 0, 0, 0, 0, 0, 0)
+    )
+    Assertions.assertArrayEquals(expectedArray, calendarArray)
+  }
+
+  @Test
+  fun generateCalendarArrayForDecember() {
+    val calendarArray: Array<IntArray> = CalendarArray().generate(YearMonth.of(2023, 12))
+    val expectedArray = arrayOf(
+      intArrayOf(0, 0, 0, 0, 0, 1, 2),
+      intArrayOf(3, 4, 5, 6, 7, 8, 9),
+      intArrayOf(10, 11, 12, 13, 14, 15, 16),
+      intArrayOf(17, 18, 19, 20, 21, 22, 23),
+      intArrayOf(24, 25, 26, 27, 28, 29, 30),
+      intArrayOf(31, 0, 0, 0, 0, 0, 0)
+    )
+    Assertions.assertArrayEquals(expectedArray, calendarArray)
+  }
 }
