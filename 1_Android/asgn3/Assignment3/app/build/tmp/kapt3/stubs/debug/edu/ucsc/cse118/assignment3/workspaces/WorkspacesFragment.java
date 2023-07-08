ee2d6da094d5967d0f2ece7d501e172019b68eee
Troys-Code <troy.schultz@chargepoint.com>
2023-07-07 18:24:53 -0700

    REdo
    redo

---
+++ 1_Android/asgn3/Assignment3/app/build/tmp/kapt3/stubs/debug/edu/ucsc/cse118/assignment3/workspaces/WorkspacesFragment.java
@@ -0,0 +1,35 @@
+package edu.ucsc.cse118.assignment3.workspaces;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000>\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J&\u0010\u0007\u001a\u0004\u0018\u00010\b2\u0006\u0010\t\u001a\u00020\n2\b\u0010\u000b\u001a\u0004\u0018\u00010\f2\b\u0010\r\u001a\u0004\u0018\u00010\u000eH\u0016J\b\u0010\u000f\u001a\u00020\u0010H\u0016J\u001a\u0010\u0011\u001a\u00020\u00102\u0006\u0010\u0012\u001a\u00020\b2\b\u0010\r\u001a\u0004\u0018\u00010\u000eH\u0016J\u0010\u0010\u0013\u001a\u00020\u00102\u0006\u0010\u0014\u001a\u00020\u0015H\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0016"}, d2 = {"Ledu/ucsc/cse118/assignment3/workspaces/WorkspacesFragment;", "Landroidx/fragment/app/Fragment;", "()V", "workspaceAdapter", "Ledu/ucsc/cse118/assignment3/workspaces/WorkspaceAdapter;", "workspacesViewModel", "Ledu/ucsc/cse118/assignment3/workspaces/WorkspacesViewModel;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onDestroyView", "", "onViewCreated", "view", "onWorkspaceClicked", "workspace", "Ledu/ucsc/cse118/assignment3/data/DataClasses$Workspace;", "app_debug"})
+public final class WorkspacesFragment extends androidx.fragment.app.Fragment {
+    private edu.ucsc.cse118.assignment3.workspaces.WorkspaceAdapter workspaceAdapter;
+    private edu.ucsc.cse118.assignment3.workspaces.WorkspacesViewModel workspacesViewModel;
+    
+    public WorkspacesFragment() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.Nullable
+    @java.lang.Override
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    @java.lang.Override
+    public void onViewCreated(@org.jetbrains.annotations.NotNull
+    android.view.View view, @org.jetbrains.annotations.Nullable
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void onWorkspaceClicked(edu.ucsc.cse118.assignment3.data.DataClasses.Workspace workspace) {
+    }
+    
+    @java.lang.Override
+    public void onDestroyView() {
+    }
+}
