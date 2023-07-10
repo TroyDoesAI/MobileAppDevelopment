5ce03a930d7c9ca929f52f5a1e1af765dc7331b4
Troys-Code <troy.schultz@chargepoint.com>
2023-07-09 18:59:56 -0700

    Added Comments and Resources and Wrote Tests for Calendar Functionality

--- 2_IOS/Assignment4/Assignment4/UI/Assignment4View.swift
+++ 2_IOS/Assignment4/Assignment4/UI/Assignment4View.swift
@@ -1,32 +1,43 @@
 import SwiftUI
 
+// This is a SwiftUI View that contains both a postfix calculator and a calendar generator.
 struct Assignment4View: View {
+    // Two state properties for the expression to be evaluated and the result of the evaluation.
     @State private var expression: String = ""
     @State private var result: String = ""
-    
+
+    // The currently selected date and the calendar generator instance.
     @State private var date: Date = Date()
     private var calendarGenerator = CalendarGenerator()
+    
+    // DateFormatter to format the date to be shown in the calendar section.
     private var dateFormatter: DateFormatter = {
         let df = DateFormatter()
         df.dateFormat = "MMMM yyyy"
         return df
     }()
     
+    // The body of the SwiftUI view.
     var body: some View {
+        // The content is stacked vertically in a VStack.
         VStack {
+            // Title for the Postfix Calculator section.
             Text("Postfix Calculator")
                 .font(.title)
                 .padding(.bottom, 20)
-
+            
+            // TextField for the user to enter the expression to be evaluated.
             TextField("Expression", text: $expression, onCommit: evaluateExpression)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal, 20)
-
+            
+            // TextField to display the result of the evaluation.
             TextField("Result", text: $result)
                 .disabled(true)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal, 20)
-
+            
+            // Evaluate and Clear buttons.
             HStack {
                 Button(action: evaluateExpression) {
                     Text("Evaluate")
@@ -45,14 +56,17 @@ struct Assignment4View: View {
             }
             .padding(.top, 20)
             
+            // Title for the Calendar Generator section.
             Text("Calendar Generator")
                 .font(.title)
                 .padding(.top, 20)
             
+            // Displays the currently selected month and year.
             Text(dateFormatter.string(from: date))
                 .font(.title2)
                 .padding(.bottom, 20)
-
+            
+            // Displays the calendar for the currently selected month.
             ForEach(generateCalendarArray(), id: \.self) { week in
                 HStack {
                     ForEach(week, id: \.self) { day in
@@ -61,7 +75,8 @@ struct Assignment4View: View {
                     }
                 }
             }
-
+            
+            // Previous, Today and Next buttons.
             HStack {
                 Button(action: previousMonth) {
                     Text("Previous")
@@ -86,12 +101,14 @@ struct Assignment4View: View {
                 }
             }
             .padding(.top, 20)
-
+            
+            // Spacer to push the content to the top of the screen.
             Spacer()
         }
         .padding()
     }
-
+    
+    // Function to evaluate the expression entered by the user.
     func evaluateExpression() {
         do {
             let calculator = PostfixCalculator()
@@ -102,16 +119,19 @@ struct Assignment4View: View {
         }
     }
 
+    // Function to clear the expression and result fields.
     func clearFields() {
         expression = ""
         result = ""
     }
     
+    // Function to generate the calendar for the currently selected month.
     func generateCalendarArray() -> [[Int]] {
         let components = Calendar.current.dateComponents([.year, .month], from: date)
         return calendarGenerator.generate(yearAndMonth: components)
     }
     
+    // Functions to change the currently selected month.
     func previousMonth() {
         date = Calendar.current.date(byAdding: .month, value: -1, to: date) ?? date
     }
@@ -125,6 +145,7 @@ struct Assignment4View: View {
     }
 }
 
+// This is a preview of the SwiftUI view for use in the Xcode canvas.
 #if !TESTING
 struct Assignment4View_Previews: PreviewProvider {
     static var previews: some View {
--- 2_IOS/Assignment4/Assignment4Tests/CalendarGeneratorTests.swift
+++ 2_IOS/Assignment4/Assignment4Tests/CalendarGeneratorTests.swift
@@ -2,7 +2,83 @@ import XCTest
 @testable import Assignment4
 
 final class CalendarGeneratorTests: XCTestCase {
-  
-  // Your tests go here
-  
+
+    var calendarGenerator: CalendarGenerator!
+
+    // Set up the test case by initializing the CalendarGenerator instance.
+    override func setUp() {
+        super.setUp()
+        calendarGenerator = CalendarGenerator()
+    }
+
+    // Utility function to convert the calendar array to a formatted string for easier comparison.
+    private func calendarToString(_ calendar: [[Int]]) -> String {
+        return calendar.map { week in week.map { day in String(format: "%02d", day) }.joined(separator: " ") }
+                        .joined(separator: "\n")
+    }
+
+    // Test case for generating the calendar of February in a non-leap year.
+    func testFebruaryNonLeapYear() {
+        let february2023 = DateComponents(year: 2023, month: 2)
+        let february2023Calendar = calendarGenerator.generate(yearAndMonth: february2023)
+        let calendarStr = calendarToString(february2023Calendar)
+        let expectedStr = """
+        29 30 31 01 02 03 04
+        05 06 07 08 09 10 11
+        12 13 14 15 16 17 18
+        19 20 21 22 23 24 25
+        26 27 28 01 02 03 04
+        05 06 07 08 09 10 11
+        """
+        XCTAssertEqual(calendarStr, expectedStr)
+    }
+
+    // Test case for generating the calendar of February in a leap year.
+    func testFebruaryLeapYear() {
+        let february2024 = DateComponents(year: 2024, month: 2)
+        let february2024Calendar = calendarGenerator.generate(yearAndMonth: february2024)
+        let calendarStr = calendarToString(february2024Calendar)
+        let expectedStr = """
+        28 29 30 31 01 02 03
+        04 05 06 07 08 09 10
+        11 12 13 14 15 16 17
+        18 19 20 21 22 23 24
+        25 26 27 28 29 01 02
+        03 04 05 06 07 08 09
+        """
+        XCTAssertEqual(calendarStr, expectedStr)
+    }
+
+    // Test case for generating the calendar of December in a non-leap year.
+    func testDecemberNonLeapYear() {
+        let december2023 = DateComponents(year: 2023, month: 12)
+        let december2023Calendar = calendarGenerator.generate(yearAndMonth: december2023)
+        let calendarStr = calendarToString(december2023Calendar)
+        let expectedStr = """
+        26 27 28 29 30 01 02
+        03 04 05 06 07 08 09
+        10 11 12 13 14 15 16
+        17 18 19 20 21 22 23
+        24 25 26 27 28 29 30
+        31 01 02 03 04 05 06
+        """
+        XCTAssertEqual(calendarStr, expectedStr)
+    }
+    
+    // Test case for generating the calendar of December in a leap year.
+    func testDecemberLeapYear() {
+        let december2024 = DateComponents(year: 2024, month: 12)
+        let december2024Calendar = calendarGenerator.generate(yearAndMonth: december2024)
+        let calendarStr = calendarToString(december2024Calendar)
+        let expectedStr = """
+        01 02 03 04 05 06 07
+        08 09 10 11 12 13 14
+        15 16 17 18 19 20 21
+        22 23 24 25 26 27 28
+        29 30 31 01 02 03 04
+        05 06 07 08 09 10 11
+        """
+        XCTAssertEqual(calendarStr, expectedStr)
+    }
 }
+
