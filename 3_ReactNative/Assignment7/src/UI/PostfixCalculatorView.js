10dab89ddef506e40901b7f917b76566b633a566
Troys-Code <troy.schultz@chargepoint.com>
2023-07-20 14:37:16 -0700

    PostFixCalculator functionality is complete
    ---------------------------|---------|----------|---------|---------|-------------------
    File                       | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    ---------------------------|---------|----------|---------|---------|-------------------
    All files                  |   97.77 |      100 |    87.5 |   97.77 |
     Assignment7               |     100 |      100 |     100 |     100 |
      App.js                   |     100 |      100 |     100 |     100 |
     Assignment7/src/Logic     |   96.29 |      100 |   66.66 |   96.29 |
      CalendarGenerator.js     |       0 |      100 |       0 |       0 | 14
      PostfixCalculator.js     |     100 |      100 |     100 |     100 |
     Assignment7/src/UI        |     100 |      100 |     100 |     100 |
      CalendarGeneratorView.js |     100 |      100 |     100 |     100 |
      PostfixCalculatorView.js |     100 |      100 |     100 |     100 |
    ---------------------------|---------|----------|---------|---------|-------------------
    Test Suites: 3 failed, 1 passed, 4 total
    Tests:       6 failed, 9 passed, 15 total
    Snapshots:   0 total
    Time:        1.037 s
    Ran all test suites.

--- 3_ReactNative/Assignment7/src/UI/PostfixCalculatorView.js
+++ 3_ReactNative/Assignment7/src/UI/PostfixCalculatorView.js
@@ -5,94 +5,113 @@
  * the express written permission of the copyright holder.
  */
 
-//import React from 'react';
-//import { View } from 'react-native';
-//
-//import PostfixCalculator from '../Logic/PostfixCalculator';
-//
-//const PostfixCalculatorView = () => {
-//  return (
-//    <View>
-//      {/* Your JSX goes here */}
-//    </View>
-//  );
-//};
-//
-//export default PostfixCalculatorView;
-
+// Importing necessary React functions and React Native components
 import React, { useState } from 'react';
 import { View, TextInput, Text, Button, StyleSheet } from 'react-native';
 
+// Importing the PostfixCalculator logic for evaluating expressions
 import PostfixCalculator from '../Logic/PostfixCalculator';
 
+// Defining the main PostfixCalculatorView component
 const PostfixCalculatorView = () => {
+
+  // Creating state variables for the expression input and its result
   const [expression, setExpression] = useState('');
   const [result, setResult] = useState('');
 
+  // Defining a function to evaluate the given expression
   const evaluateExpression = () => {
     try {
+      // Attempt to evaluate the expression using the imported PostfixCalculator
       const evaluation = PostfixCalculator.evaluate(expression);
+      // If successful, set the result to the evaluated value
       setResult(String(evaluation));
-    } catch (e) {
-      setResult(e);  // Set the error message directly without transforming it
+    } catch (error) {
+      // If there's an error in evaluation, display the error message
+      setResult(`Error: ${error.message}`);
     }
   };
 
+  // Defining a function to clear both the expression and result fields
   const clearFields = () => {
     setExpression('');
     setResult('');
   };
 
+  // Rendering the main component UI
   return (
     <View style={styles.container}>
+      // Header section containing the calculator's title
       <View style={styles.header}>
         <Text style={styles.title}>Postfix Calculator</Text>
       </View>
+
+      // Input field for users to type in their postfix expression
       <TextInput
         style={styles.input}
         value={expression}
-        onChangeText={setExpression}
-        placeholder="expression"
+        onChangeText={setExpression} // Function to update the expression state variable
+        placeholder="Expression"
+        accessibilityLabel="expression"  // Label for testing purposes
       />
+
+      // Display field for the calculated result (or error message)
       <TextInput
         style={styles.input}
         value={result}
-        editable={false}
-        placeholder="result"
+        editable={false} // This input field is read-only
+        placeholder="Result"
+        accessibilityLabel="result" // Label for testing purposes
       />
+
+      // Container for buttons
       <View style={styles.buttonContainer}>
-        <Button title="evaluate" onPress={evaluateExpression} color="#007AFF" />
-        <Button title="clear" onPress={clearFields} color="#FF3B30" />
+        // "Evaluate" button to trigger the expression evaluation
+        <Button
+            title="evaluate"
+            onPress={evaluateExpression}
+            color="#007AFF"
+            accessibilityLabel="evaluate" // Label for testing purposes
+        />
+        // "Clear" button to reset both expression and result fields
+        <Button
+            title="clear"
+            onPress={clearFields}
+            color="#FF3B30"
+            accessibilityLabel="clear" // Label for testing purposes
+        />
       </View>
     </View>
   );
 };
 
+// Styling for the components using React Native's StyleSheet
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    padding: 20, // Outer padding for the main container
   },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
+    flexDirection: 'row', // Layout children in a row
+    alignItems: 'center', // Center children vertically
+    marginBottom: 20, // Margin at the bottom
   },
   title: {
-    fontSize: 24,
-    marginLeft: 10,
+    fontSize: 24, // Font size for the title
+    marginLeft: 10, // Left margin to give some spacing
   },
   input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
+    borderWidth: 1, // Width of the border around input
+    borderColor: '#ccc', // Color of the border
+    borderRadius: 5, // Rounded corners
+    padding: 10, // Inner padding
+    marginBottom: 10, // Bottom margin
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
+    flexDirection: 'row', // Layout buttons in a row
+    justifyContent: 'space-between', // Distribute buttons evenly
+    marginTop: 20, // Margin at the top
   },
 });
 
+// Exporting the component to be used in other parts of the application
 export default PostfixCalculatorView;
