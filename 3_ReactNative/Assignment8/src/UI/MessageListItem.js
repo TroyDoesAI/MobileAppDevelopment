f92d5335dce9c8e997bcd7aa45049942fc6fd4d5
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-07-23 01:03:49 -0700

    Refactor after getting messages going well
    changed to dataclasses to make more sense

--- 3_ReactNative/Assignment8/src/UI/MessageListItem.js
+++ 3_ReactNative/Assignment8/src/UI/MessageListItem.js
@@ -9,6 +9,9 @@ const MessageListItem = ({ message, navigation }) => {
     return (
         <TouchableOpacity onPress={handlePress}>
             <View style={styles.container}>
+                <Text style={styles.posterName} accessibilityLabel={message.member.name}>
+                    {message.member.name}
+                </Text>
                 <Text style={styles.item} accessibilityLabel={message.content}>
                     {message.content}
                 </Text>
@@ -19,10 +22,14 @@ const MessageListItem = ({ message, navigation }) => {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
+        flexDirection: 'column', // Change to column to stack name above message
         padding: 10,
         borderBottomWidth: 1,
         borderBottomColor: '#ddd',
+    },
+    posterName: {
+        fontWeight: 'bold',   // Give the poster name some weight
+        marginBottom: 5,      // Add some space between name and message
     },
     item: {
         paddingLeft: 10,
