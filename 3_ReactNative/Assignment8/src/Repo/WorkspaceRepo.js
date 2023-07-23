ff85c45fbd348aba66e7531120680605e4aeb5ac
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-07-23 15:14:55 -0700

    Added Comments explaining these files role

--- 3_ReactNative/Assignment8/src/Repo/WorkspaceRepo.js
+++ 3_ReactNative/Assignment8/src/Repo/WorkspaceRepo.js
@@ -1,9 +1,38 @@
+// WorkspaceRepo.js
+
+// This utility function is responsible for fetching the list of workspaces from the Workspaces.json file.
+
+// Import the local JSON data.
+const workspacesJson = require('../Resources/Workspaces.json');
+
+/**
+ * Fetches the list of workspaces.
+ *
+ * @returns {Array} - A list of workspaces.
+ * @throws Will throw an error if there's an issue parsing the JSON data.
+ */
 export const GET_WORKSPACES = async () => {
   try {
-    const workspacesJson = require('../Resources/Workspaces.json');
-    // TODO Convert your json into your JavaScript objects, if needed?
+    // Return the parsed JSON data as the list of workspaces.
     return workspacesJson;
   } catch (error) {
     console.error('Error while parsing json:', error);
   }
 };
+
+/*
+SUMMARY:
+
+The WorkspaceRepo.js module is designed to serve as a data access layer for the list of workspaces from the Workspaces.json file. 
+Its main task is to fetch workspace data from the local Workspaces.json file.
+
+The `GET_WORKSPACES` function doesn't take any arguments and returns an array of workspaces. If there's an error during data retrieval 
+or processing (e.g., issues with parsing the JSON), the error gets logged to the console.
+
+INTERACTION WITH OTHER FILES:
+
+1. Components or other modules that need the list of workspaces will import this utility function and invoke it.
+2. The function's result (the list of workspaces) can then be utilized by the calling component or module for its purposes, 
+   such as display or further processing.
+3. Callers should correctly handle the function's output, managing potential errors and interpreting the data as needed.
+*/
