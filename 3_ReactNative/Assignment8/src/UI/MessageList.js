846124b99a6c2fb2ca01bcf1af753925e550e90e
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-07-24 11:16:43 -0700

    Use Callbacks to memoize the extracted headers to prevent rerendering
    - Refactored MessageList.js:
      - Extracted header components (`HeaderTitle` and `HeaderLeft`) out of the main `MessageList` component to avoid defining them during each render.
      - Used `useCallback` to memoize the extracted header components, ensuring they aren't recreated on every render.
      - Adjusted `useLayoutEffect` to use the memoized header components.

--- 3_ReactNative/Assignment8/src/UI/MessageList.js
+++ 3_ReactNative/Assignment8/src/UI/MessageList.js
@@ -1,18 +1,37 @@
-import React, {useLayoutEffect, useContext, useEffect} from 'react';
+import React, {
+  useLayoutEffect,
+  useContext,
+  useEffect,
+  useCallback,
+} from 'react';
 import {FlatList, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import MessageListItem from './MessageListItem';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {MessageContext} from '../Model/MessageViewModel';
 import {GET_MESSAGES_FOR_CHANNEL} from '../Repo/MessageRepo';
 
+const HeaderTitle = ({channelName}) => (
+  <Text numberOfLines={1} ellipsizeMode="tail" style={styles.title}>
+    {channelName}
+  </Text>
+);
+
+const HeaderLeft = ({navigation, workspaceName}) => (
+  <TouchableOpacity
+    onPress={() => navigation.goBack()}
+    style={styles.backButton}
+    accessibilityLabel="back to channels">
+    <Icon name="chevron-left" size={25} color="blue" />
+    <Text style={styles.backText}>{workspaceName}</Text>
+  </TouchableOpacity>
+);
+
 const MessageList = ({route, navigation}) => {
   const {messages, setMessages} = useContext(MessageContext);
   const {channelName, workspaceName} = route.params;
 
   const sortMessagesByDate = msgs => {
-    return messages
-      .slice()
-      .sort((a, b) => new Date(b.posted) - new Date(a.posted));
+    return msgs.slice().sort((a, b) => new Date(b.posted) - new Date(a.posted));
   };
 
   const sortedMessages = sortMessagesByDate(messages);
@@ -23,28 +42,25 @@ const MessageList = ({route, navigation}) => {
       setMessages(fetchedMessages);
     };
     fetchMessagesForChannel(route.params.channelId);
-  }, [route.params, setMessages]); // check
+  }, [route.params, setMessages]);
+
+  const memoizedHeaderTitle = useCallback(
+    () => <HeaderTitle channelName={channelName} />,
+    [channelName],
+  );
+  const memoizedHeaderLeft = useCallback(
+    () => <HeaderLeft navigation={navigation} workspaceName={workspaceName} />,
+    [navigation, workspaceName],
+  );
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: () => (
-        <Text numberOfLines={1} ellipsizeMode="tail" style={styles.title}>
-          {channelName}
-        </Text>
-      ),
-      headerLeft: () => (
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          style={styles.backButton}
-          accessibilityLabel="back to channels">
-          <Icon name="chevron-left" size={25} color="blue" />
-          <Text style={styles.backText}>{workspaceName}</Text>
-        </TouchableOpacity>
-      ),
+      headerTitle: memoizedHeaderTitle,
+      headerLeft: memoizedHeaderLeft,
       headerTitleAlign: 'center',
       headerBackTitleVisible: false,
     });
-  }, [navigation, channelName, workspaceName]);
+  }, [navigation, memoizedHeaderTitle, memoizedHeaderLeft]);
 
   return (
     <FlatList
