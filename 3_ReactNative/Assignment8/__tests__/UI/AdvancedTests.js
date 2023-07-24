b625644e1663d8ebc869ded0dedc48503a0d4a37
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-07-24 16:13:25 -0700

    Updated Advanced Test Code

--- 3_ReactNative/Assignment8/__tests__/UI/AdvancedTests.js
+++ 3_ReactNative/Assignment8/__tests__/UI/AdvancedTests.js
@@ -5,7 +5,6 @@ import {ChannelContext} from '../../src/Model/ChannelViewModel';
 
 const mockContextValue = {
   loadChannelsForWorkspace: jest.fn(),
-  // ... other methods and values from the context, if any
 };
 
 const customRender = (ui, options) =>
@@ -16,7 +15,16 @@ const customRender = (ui, options) =>
     options,
   );
 
+/**
+ * This test suite focuses on the `WorkspaceListItem` component.
+ */
 describe('WorkspaceListItem', () => {
+  /**
+   * This test checks the behavior when there are no messages in a workspace.
+   * 1. Mock a workspace without messages.
+   * 2. Render the component with this mock data.
+   * 3. Ensure that the text "Latest:" is present, indicating no recent messages.
+   */
   it('shows an empty string for latest message when there are no messages', () => {
     const mockNavigation = {
       navigate: jest.fn(),
@@ -37,9 +45,116 @@ describe('WorkspaceListItem', () => {
     );
 
     debug();
-
     expect(queryByText(/Latest:/i)).not.toBeNull();
   });
 
-  // Add any other advanced tests below
+  /**
+   * Group of tests focusing on how elapsed time since the most recent message is displayed.
+   */
+  describe('Time Display Tests', () => {
+    /**
+     * Parametrized test to ensure correct time formatting.
+     * - For a message 3 seconds ago, should show "3 secs"
+     * - For a message 1 minute ago, should show "1 mins"
+     * - For a message 2 hours ago, should show "2 hours"
+     * - For a message 2 days ago, should show "2 days"
+     *
+     * For each scenario:
+     * 1. Mock a workspace with the specified most recent message time.
+     * 2. Render the component.
+     * 3. Check if the expected formatted time text is displayed.
+     */
+    it.each([
+      [new Date(Date.now() - 3000), '3 secs'],
+      [new Date(Date.now() - 60000), '1 mins'],
+      [new Date(Date.now() - 7200000), '2 hours'],
+      [new Date(Date.now() - 86400000 * 2), '2 days'],
+    ])('correctly formats elapsed time', (messageDate, expectedText) => {
+      const workspaceWithMessage = {
+        name: 'Workspace with Message',
+        channels: [],
+        uniquePosters: 2, // Mock value
+        mostRecentMessage: messageDate,
+      };
+
+      const {getByText} = customRender(
+        <WorkspaceListItem workspace={workspaceWithMessage} navigation={{}} />,
+      );
+
+      expect(getByText(`Latest: ${expectedText}`)).not.toBeNull();
+    });
+  });
+
+  /**
+   * Group of tests that will focus on how numeric indicators (counts) are displayed.
+   * TODO: Complete this test by mocking a workspace with various counts and
+   * check if they're displayed correctly.
+   */
+  describe('Numeric Indicators Test', () => {
+    it('matches numeric indicators in WorkspaceListItem', () => {
+      // TODO: Implementation
+    });
+  });
+
+  /**
+   * Group of tests focusing on the display of unique posters.
+   * TODO: Complete this test by mocking a workspace with various unique posters
+   * and check if they're displayed correctly.
+   */
+  describe('Unique Posters Test', () => {
+    it('displays the correct count of unique posters', () => {
+      // TODO: Implementation
+    });
+  });
+
+  /**
+   * Tests focusing on accessibility.
+   * Ensures that dynamic accessibility identifiers are set correctly.
+   * TODO: Complete this test by rendering the component and checking
+   * for appropriate accessibility labels.
+   */
+  describe('Accessibility Identifiers Test', () => {
+    it('sets dynamic accessibility identifiers', () => {
+      // TODO: Implementation
+    });
+  });
+
+  /**
+   * Tests the behavior for workspaces with no channels.
+   * 1. Mock a workspace without any channels.
+   * 2. Render the component.
+   * 3. Ensure that the channel count is displayed as 0.
+   * 4. Ensure that the recent message is blank.
+   */
+  describe('No Channel Test', () => {
+    it('shows zero channels and a blank recent message for workspaces with no channels', () => {
+      const workspaceWithNoChannels = {
+        name: 'Empty Workspace',
+        channels: [],
+        uniquePosters: 0,
+        mostRecentMessage: null,
+      };
+
+      const {getByText} = customRender(
+        <WorkspaceListItem
+          workspace={workspaceWithNoChannels}
+          navigation={{}}
+        />,
+      );
+
+      expect(getByText(/Channels: 0/i)).not.toBeNull();
+      expect(getByText(/Latest:/i)).not.toBeNull();
+    });
+  });
+
+  /**
+   * Tests the display of the most recent message from all channels in a workspace.
+   * TODO: Complete this test by mocking a workspace with channels having messages
+   * of various dates, then check that the latest message is displayed correctly.
+   */
+  describe('Most Recent Message Test', () => {
+    it('displays the most recent message across all channels', () => {
+      // TODO: Implementation
+    });
+  });
 });
