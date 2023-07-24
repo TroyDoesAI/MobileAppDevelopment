fd0b64a7b5abcf2feaa0cfe324c72c66a3099af5
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-07-24 13:02:51 -0700

    Update ChannelRepo.js
    Implemented Data Class Integration for ChannelRepo:
    
    - Enhanced `ChannelRepo.js` to transform raw JSON channel and message data into instances of `Channel` and `Message` classes from `DataClasses.js`.
    - Ensured that channels fetched via `GET_CHANNELS_FOR_WORKSPACE` are now `Channel` instances, allowing for easier access to computed properties like `uniquePosters` and `mostRecentMessage`.
    - Streamlined data retrieval and processing to provide a consistent, object-oriented interface for channel and message data throughout the application.
    - This change further solidifies the OO approach, aligning with the recent modifications in `WorkspaceRepo.js`, and making data handling more uniform and scalable.

--- 3_ReactNative/Assignment8/src/Repo/ChannelRepo.js
+++ 3_ReactNative/Assignment8/src/Repo/ChannelRepo.js
@@ -3,26 +3,51 @@
 // This utility function is responsible for fetching channels associated
 // with a given workspace from the Workspaces.json file.
 
-// Import the local JSON data.
+// // Import the local JSON data.
+// const workspacesJson = require('../Resources/Workspaces.json');
+
+// /**
+//  * Fetches the channels associated with a specific workspace ID.
+//  *
+//  * @param {number} workspaceId - The ID of the workspace to retrieve channels for.
+//  * @returns {Array} - A list of channels associated with the given workspace,
+//  * or an empty array if the workspace is not found.
+//  * @throws Will throw an error if there's an issue parsing the JSON data.
+//  */
+
+// export const GET_CHANNELS_FOR_WORKSPACE = async workspaceId => {
+//   // Search through the workspaces to find a workspace with the given ID.
+//   const workspace = workspacesJson.find(ws => ws.id === workspaceId);
+
+//   // If a workspace is found with the matching ID, return its associated channels.
+//   // If not found, return an empty array.
+//   return workspace.channels;
+// };
+
+import { Workspace, Channel, Message, Member } from '../Model/DataClasses'; // Adjust the path as necessary
+
 const workspacesJson = require('../Resources/Workspaces.json');
 
-/**
- * Fetches the channels associated with a specific workspace ID.
- *
- * @param {number} workspaceId - The ID of the workspace to retrieve channels for.
- * @returns {Array} - A list of channels associated with the given workspace,
- * or an empty array if the workspace is not found.
- * @throws Will throw an error if there's an issue parsing the JSON data.
- */
-
 export const GET_CHANNELS_FOR_WORKSPACE = async workspaceId => {
-  // Search through the workspaces to find a workspace with the given ID.
   const workspace = workspacesJson.find(ws => ws.id === workspaceId);
 
-  // If a workspace is found with the matching ID, return its associated channels.
-  // If not found, return an empty array.
-  return workspace.channels;
+  if (!workspace) return [];
+
+  const channels = workspace.channels.map(channelData => {
+    const messages = channelData.messages.map(messageData => {
+      return new Message(
+        messageData.id, 
+        messageData.content, 
+        messageData.posted, 
+        new Member(messageData.member.id, messageData.member.name)
+      );
+    });
+    return new Channel(channelData.id, channelData.name, messages);
+  });
+  
+  return channels;
 };
+
 
 /*
 SUMMARY:
