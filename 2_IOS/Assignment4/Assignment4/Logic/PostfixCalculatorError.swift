f9086b8cc4e6ac7d671da22c2ad3ff62901e9c69
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-07-27 10:20:36 -0700

    Removed old error handling
    100% coverage

--- 2_IOS/Assignment4/Assignment4/Logic/PostfixCalculatorError.swift
+++ 2_IOS/Assignment4/Assignment4/Logic/PostfixCalculatorError.swift
@@ -6,14 +6,5 @@ enum PostfixCalculatorError: Error, LocalizedError {
     case missingOperand
     case invalidOperator
     case notImplemented(String)
-
-    var errorDescription: String? {
-        switch self {
-        case .malformedExpression: return "Malformed expression"
-        case .divisionByZero: return "Division by zero"
-        case .missingOperand: return "Missing operand"
-        case .invalidOperator: return "Invalid operator"
-        case .notImplemented(let msg): return msg
-        }
-    }
 }
+
