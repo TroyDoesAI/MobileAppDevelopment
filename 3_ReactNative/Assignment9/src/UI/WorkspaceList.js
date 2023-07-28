fbbda03b076d68964bf9131de46ffd70197bc117
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-07-27 22:08:29 -0700

    Reset Button calls /reset api
    Bug List:
    
    - Add messages does not update the message count on adding, must go back and forth a page that needs to be fixed
    
    - clicking reset button makes it where the only option is to log out as it crashes navigation?

--- 3_ReactNative/Assignment9/src/UI/WorkspaceList.js
+++ 3_ReactNative/Assignment9/src/UI/WorkspaceList.js
@@ -1,10 +1,11 @@
 // WorkspaceList.js
-import React, {useContext, useState, useEffect} from 'react';
-import {FlatList, Button} from 'react-native';
+
+import React, { useContext, useState, useEffect } from 'react';
+import { FlatList, Button } from 'react-native';
 import WorkspaceListItem from './WorkspaceListItem';
-import {WorkspaceContext} from '../Model/WorkspaceViewModel';
+import { WorkspaceContext } from '../Model/WorkspaceViewModel';
 import AuthContext from '../Model/AuthContext';
-import {GET_WORKSPACES} from '../Repo/WorkspaceRepo';
+import { GET_WORKSPACES } from '../Repo/WorkspaceRepo';
 
 function sortWorkspacesByDate(workspaces) {
   return workspaces
@@ -12,19 +13,28 @@ function sortWorkspacesByDate(workspaces) {
     .sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate));
 }
 
-const WorkspaceList = ({navigation}) => {
-  const {token, signOut} = useContext(AuthContext);
-  const {workspaces} = useContext(WorkspaceContext);
+const WorkspaceList = ({ navigation }) => {
+  const { token, signOut } = useContext(AuthContext);
+  const { workspaces } = useContext(WorkspaceContext);
   const [fetchedWorkspaces, setFetchedWorkspaces] = useState([]);
 
   useEffect(() => {
     if (token) {
       GET_WORKSPACES(token).then(data => setFetchedWorkspaces(data));
-      // Removed the .catch() block
     }
   }, [token]);
 
-  // Custom header with logout button
+  const handleReset = async () => {
+    await fetch('https://cse118.com/api/v2/reset', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+  // Custom header with logout and reset button
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
@@ -33,12 +43,20 @@ const WorkspaceList = ({navigation}) => {
             signOut();
             navigation.reset({
               index: 0,
-              routes: [{name: 'Login'}],
+              routes: [{ name: 'Login' }],
             });
           }}
           title="Logout"
           color="#000"
           accessibilityLabel="logout"
+        />
+      ),
+      headerRight: () => (
+        <Button
+          onPress={handleReset}
+          title="Reset"
+          color="#000"
+          accessibilityLabel="reset"
         />
       ),
       headerBackTitleVisible: false,
@@ -53,7 +71,7 @@ const WorkspaceList = ({navigation}) => {
     <FlatList
       data={sortedWorkspaces}
       keyExtractor={item => item.id.toString()}
-      renderItem={({item}) => (
+      renderItem={({ item }) => (
         <WorkspaceListItem workspace={item} navigation={navigation} />
       )}
       initialNumToRender={20}
