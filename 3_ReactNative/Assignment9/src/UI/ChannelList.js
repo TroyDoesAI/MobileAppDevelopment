cf3ca62aa6663f6ca81ad737579ea1387fbe3d4a
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-07-27 20:07:13 -0700

    Code coverage is nearly 100%
    ------------------------|---------|----------|---------|---------|-------------------
    File                    | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    ------------------------|---------|----------|---------|---------|-------------------
    All files               |     100 |      100 |   97.01 |     100 |
     Assignment9            |     100 |      100 |     100 |     100 |
      App.js                |     100 |      100 |     100 |     100 |
     Assignment9/src/Model  |     100 |      100 |   88.88 |     100 |
      AuthContext.js        |     100 |      100 |       0 |     100 |
      AuthProvider.js       |     100 |      100 |     100 |     100 |
      ChannelViewModel.js   |     100 |      100 |     100 |     100 |
      DataClasses.js        |     100 |      100 |     100 |     100 |
      DataUtil.js           |     100 |      100 |     100 |     100 |
      MessageViewModel.js   |     100 |      100 |     100 |     100 |
      WorkspaceViewModel.js |     100 |      100 |     100 |     100 |
     Assignment9/src/Repo   |     100 |      100 |     100 |     100 |
      ChannelRepo.js        |     100 |      100 |     100 |     100 |
      MembersRepo.js        |     100 |      100 |     100 |     100 |
      MessageRepo.js        |     100 |      100 |     100 |     100 |
      WorkspaceRepo.js      |     100 |      100 |     100 |     100 |
     Assignment9/src/UI     |     100 |      100 |     100 |     100 |
      ChannelList.js        |     100 |      100 |     100 |     100 |
      ChannelListItem.js    |     100 |      100 |     100 |     100 |
      Login.js              |     100 |      100 |     100 |     100 |
      MessageDetail.js      |     100 |      100 |     100 |     100 |
      MessageList.js        |     100 |      100 |     100 |     100 |
      MessageListItem.js    |     100 |      100 |     100 |     100 |
      WorkspaceList.js      |     100 |      100 |     100 |     100 |
      WorkspaceListItem.js  |     100 |      100 |     100 |     100 |
    ------------------------|---------|----------|---------|---------|-------------------
    Test Suites: 1 failed, 1 passed, 2 total
    Tests:       4 failed, 8 passed, 12 total
    Snapshots:   0 total
    Time:        15.215 s
    Ran all test suites.

--- 3_ReactNative/Assignment9/src/UI/ChannelList.js
+++ 3_ReactNative/Assignment9/src/UI/ChannelList.js
@@ -1,7 +1,8 @@
+// ChannelList.js
+
 import React, {useLayoutEffect, useCallback} from 'react';
-import {FlatList, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {FlatList, Text, StyleSheet} from 'react-native';
 import ChannelListItem from './ChannelListItem';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import {ChannelContext} from '../Model/ChannelViewModel';
 
 const ChannelList = ({navigation, route}) => {
@@ -17,27 +18,14 @@ const ChannelList = ({navigation, route}) => {
     [workspaceName],
   );
 
-  const HeaderLeft = useCallback(
-    () => (
-      <TouchableOpacity
-        onPress={() => navigation.goBack()}
-        style={styles.backButton}
-        accessibilityLabel="back to workspaces">
-        <Icon name="chevron-left" size={25} color="blue" />
-        <Text style={styles.backText}>Workspaces</Text>
-      </TouchableOpacity>
-    ),
-    [navigation],
-  );
-
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: HeaderTitle,
-      headerLeft: HeaderLeft,
       headerTitleAlign: 'center',
       headerBackTitleVisible: false,
+      headerBackAccessibilityLabel: 'back to workspaces', // Setting the accessibility label for the default back button
     });
-  }, [navigation, HeaderTitle, HeaderLeft]);
+  }, [navigation, HeaderTitle]);
 
   return (
     <FlatList
@@ -59,16 +47,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 10,
     fontWeight: 'bold',
-  },
-  backButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft: 10,
-  },
-  backText: {
-    fontSize: 10,
-    marginLeft: 5,
-    color: 'blue',
   },
 });
 
